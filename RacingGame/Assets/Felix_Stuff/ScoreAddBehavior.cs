b88afe6152ff8048de8d5f915be9f119ee95ccca
Flexflext <felix.hereth.fh@googlemail.com>
2021-05-08 18:52:30 +0200

    InGameUI

---
+++ RacingGame/Assets/Felix_Stuff/ScoreAddBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreAddBehavior : MonoBehaviour
+{
+    public float ScoreToAdd;
+
+    [SerializeField] private TMP_Text score;
+
+    private void Start()
+    {
+        score.text = "+" + ScoreToAdd.ToString();
+    }
+
+    public void DestroyGameObject()
+    {
+        Destroy(this.gameObject);
+    }
+}
