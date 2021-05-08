1504b27d70aa01741de2f10ae529e9f6737f31f2
GreenGift <aschleeh@gmx.de>
2021-05-08 18:53:35 +0200

    ref(score): satrted Score Script

---
+++ RacingGame/Assets/Adrian_Stuff/Scripts/ScoreManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+
+    //private 
+
+    private float _score;
+
+    [SerializeField]
+    private int _bonusForEnemyKilled;
+
+    [SerializeField]
+    private int _amountPerMeter;
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    public void EnemyDied()
+    {
+        _score += _bonusForEnemyKilled;
+    }
+
+    public void AddScore(float meters)
+    {
+        _score += meters * _amountPerMeter;
+    }
+
+}
