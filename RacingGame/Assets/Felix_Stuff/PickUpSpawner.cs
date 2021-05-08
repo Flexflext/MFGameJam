8606ae09c5ff9c713e1fe0609862a53801ccc597
Flexflext <felix.hereth.fh@googlemail.com>
2021-05-08 16:07:19 +0200

    PlayerShit

---
+++ RacingGame/Assets/Felix_Stuff/PickUpSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpSpawner : MonoBehaviour
+{
+    [SerializeField] float maxTimeBetweenSpawns;
+    [SerializeField] private float minTimeBetweenSpawns;
+    private float currentTime;
+
+    [SerializeField] private GameObject pickUpPrefab;
+
+    [SerializeField] private Transform[] spawnPoints;
+
+    private void Start()
+    {
+        currentTime = maxTimeBetweenSpawns;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+        }
+        else
+        {
+            SpawnPickUp();
+            currentTime = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
+        }
+    }
+
+    private void SpawnPickUp()
+    {
+        Vector3 positionToSpawn = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
+
+        Instantiate(pickUpPrefab, positionToSpawn, Quaternion.identity, this.transform);
+    }
+}
