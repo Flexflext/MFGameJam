8bd78dfa95265ca5e1dd77530edc53c441590573
Flexflext <felix.hereth.fh@googlemail.com>
2021-05-08 13:29:28 +0200

    start  player controller

---
+++ RacingGame/Assets/Felix_Stuff/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void HandleInput()
+    {
+
+    }
+}
